1075c7908d57241a8e53578133c9a3e0324cf1cf
Daniel Lu <ludaniel20110@outlook.com>
2022-05-15 12:06:14 +0000

    add lang support

---
+++ colorshelll.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main (void) {
+    printf("\033[31m■ Red Foreground\n\033[32m■ Green Foreground\n\033[33m■ Yellow Foreground\n\033[34m■ Blue Foreground\n\033[35m■ Magenta Foreground\n\033[36m■ Cyan Foreground\n\033[37m■ White Foreground\n\033[30m■ Black Foreground\n");
+    return 0;
+}
