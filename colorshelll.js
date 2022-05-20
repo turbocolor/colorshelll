07e020edfcc92c6a061c8a5bd35aeb52c3fa7cc6
Daniel Lu <95860724+DWhirlpool@users.noreply.github.com>
2022-05-20 17:55:42 +0800

    add node.js version

---
+++ colorshelll.js
@@ -0,0 +1,9 @@
+const repl = require('node:repl');
+const local = repl.start();
+
+console.log('\033[31m■ Red Foreground\n\033[32m■ Green Foreground\n\033[33m■ Yellow Foreground\n\033[34m■ Blue Foreground\n\033[35m■ Magenta Foreground\n\033[36m■ Cyan Foreground\n\033[37m■ White Foreground\n\033[30m■ Black Foreground\n\033[34m')
+
+local.on('exit', () => {
+    console.log('exiting colorshelll');
+    process.exit();
+});  
