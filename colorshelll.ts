f11a2214ae4f14073dc4dceccc3ec0d0845bfa0d
Daniel Lu <95860724+DWhirlpool@users.noreply.github.com>
2022-06-22 13:04:42 +0000

    0.0.6

--- colorshelll.ts
+++ colorshelll.ts
@@ -1,8 +1,13 @@
-const repl = require('node:repl');
 const os = require('node:os');
 const gradient = require('gradient-string');
-const local = repl.start();
 const osinfo = (os.hostname()+'-'+os.platform()+'-'+os.version())
+const catpt1 = `
+\\    /\\
+ )  ( ')`
+const catpt2 = `
+(  /  )
+ \\(__)|
+`
 
 let startgradient = gradient('red','blue');
 let ocean = gradient(['#86efac','#3b82f6','#9333ea']);
@@ -26,10 +31,10 @@ let str7 = forest("▬▬▬▬▬▬Forest▬▬▬▬▬▬")
 let str8 = air("▬▬▬▬▬▬Air▬▬▬▬▬▬")
 let str9 = water("▬▬▬▬▬▬Water▬▬▬▬▬▬")
 
-console.log("colorshelll-node-ts --start\n")
-console.log(startstr);
+console.log("colorshelll-node --start\n")
+console.log('\033[1m' + startstr + '\033[0m');
 console.log('\033[31m■ Red Foreground\n\033[32m■ Green Foreground\n\033[33m■ Yellow Foreground\n\033[34m■ Blue Foreground\n\033[35m■ Magenta Foreground\n\033[36m■ Cyan Foreground\n\033[37m■ White Foreground\n\033[30m■ Black Foreground\n\033[34m')
-console.log(secondstr);
+console.log('\033[1m' + secondstr + '\033[0m');
 console.log(str1);
 console.log(str2);
 console.log(str3);
@@ -39,9 +44,6 @@ console.log(str6);
 console.log(str7);
 console.log(str8);
 console.log(str9);
-console.log('@turbocolor/colorshelll@0.0.5 on ', osinfo);
 
-local.on('exit', () => {
-    console.log('exiting colorshelll');
-    process.exit();
-});
+console.log(catpt1,'@turbocolor/colorshelll@0.0.6 on ', osinfo, catpt2);
+process.exit();
